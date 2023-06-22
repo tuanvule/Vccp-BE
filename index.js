2c425bd514b8bde599f56e3170fcbef4df0d8d3c
letuanvu <ngovipmo1@gmail.com>
2023-06-22 09:11:00 +0700

    5

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ const db = require('./config/db')
 
 db.connect()
 
-app.use(cors({
-    origin: "http://localhost:3000/", // Vervang door de juiste oorsprong
-    methods: "GET, POST, PUT, DELETE", // Vervang door de toegestane methoden
-    allowedHeaders: "Content-Type", // Vervang door de toegestane headers
-    credentials: true, // Schakel cookies en verificatie in indien nodig
-    optionsSuccessStatus: 200
-}))
+// app.use(cors())
 app.use(express.json())
 // app.use((req, res) => {
 //     res.header("Access-Control-Allow-Origin", 'http://localhost:3000');

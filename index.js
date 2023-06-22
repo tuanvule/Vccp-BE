7dc026cad7b55b550da9dea7f23682b3ea1a0914
letuanvu <ngovipmo1@gmail.com>
2023-06-22 08:53:41 +0700

    s

--- index.js
+++ index.js
@@ -11,8 +11,26 @@ db.connect()
 
 app.use(cors())
 app.use(express.json())
+// app.use((req, res) => {
+//     res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Options");
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+// })
 routes(app)
 
-app.listen(4000, () => {
-    
+app.listen('4000', () => {
+
 })
+
+
+
+// var http = require('http');
+// var express = require('express');
+// var app = express();
+
+// // your express configuration here
+
+// var httpServer = http.createServer(app);
+
+// httpServer.listen('vccp-be.vercel.app');

06aa40bafa485155711faaf80e6f6e04a1a5f73b
letuanvu <ngovipmo1@gmail.com>
2023-06-23 09:35:08 +0700

    3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ db.connect()
 
 // app.use(cors())
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://vccp-be.vercel.app/");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();

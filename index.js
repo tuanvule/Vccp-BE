2789acba0aadbe406bae5c15275cc8bc114e5997
letuanvu <ngovipmo1@gmail.com>
2023-06-26 08:40:17 +0700

    a

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ db.connect()
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 })
 app.use(express.json())
